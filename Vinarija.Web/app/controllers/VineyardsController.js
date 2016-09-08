3d1f00aa716a811d2d767c0a62d64b603a68a051
engineers-zorica <zorica@enginee.rs>
2016-09-08 15:27:45 +0200

    Added website

---
+++ Vinarija.Web/app/controllers/VineyardsController.js
@@ -0,0 +1,2 @@
+﻿app.controller('VineyardsController', function ($scope) {
+});
---
+++ Vinarija.Web/app/controllers/VisitWineryController.js
@@ -0,0 +1,23 @@
+﻿app.controller('VisitWineryController', function ($scope) {
+
+    $scope.map = {
+        center: {
+            latitude: 39.8282,
+            longitude: -98.5795
+        },
+        zoom: 10
+    };
+
+    $scope.marker = {
+        id: 0,
+        coords: {
+            latitude: 39.8282,
+            longitude: -98.5795
+        },
+        options: {
+            icon: '/assets/images/map-marker.png'
+        }
+    };
+
+    $scope.options = {};
+});
