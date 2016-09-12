7b846775aa80d7d1bd63b0e74e119749b32bebdd
engineers-zorica <zorica@enginee.rs>
2016-09-12 16:32:15 +0200

    Styling

--- Vinarija.Web/app/controllers/RedWinesController.js
+++ Vinarija.Web/app/controllers/RedWinesController.js
@@ -1,6 +1,9 @@
 ﻿app.controller('RedWinesController', function ($scope, $timeout) {
 
     $(document).ready(function () {
+        if ($('html').hasClass("fp-enabled")) {
+            $.fn.fullpage.destroy('all');
+        }
         $('#fullpage').fullpage();
     });
 
--- Vinarija.Web/app/controllers/WhiteWinesController.js
+++ Vinarija.Web/app/controllers/WhiteWinesController.js
@@ -1,6 +1,9 @@
 ﻿app.controller('WhiteWinesController', function ($scope, $timeout) {
 
     $(document).ready(function () {
+        if ($('html').hasClass("fp-enabled")) {
+            $.fn.fullpage.destroy('all');
+        }
         $('#fullpage-white').fullpage();
     });
 
