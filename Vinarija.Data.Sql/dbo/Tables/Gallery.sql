f0459e6bb25b3fc61c37fc6026f14f9e5e070d0f
Nikola Zivkovic <nikola.z@enginee.rs>
2016-09-21 09:35:34 +0200

    DB patches

--- Vinarija.Data.Sql/dbo/Tables/Gallery.sql
+++ Vinarija.Data.Sql/dbo/Tables/Gallery.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Gallery] (
     [Id]        INT            IDENTITY (1, 1) NOT NULL,
     [FilePath]  NVARCHAR (100) NOT NULL,
-    [SortOrder] INT            NULL,
+    [SortOrder] INT            NOT NULL,
     CONSTRAINT [PK_Gallery] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
