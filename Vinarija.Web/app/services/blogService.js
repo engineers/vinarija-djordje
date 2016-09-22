d6fed18ee3d5583a6a5afcf951470ca8d2342d1f
engineers-zorica <zorica@enginee.rs>
2016-09-22 10:16:29 +0200

    Fixed problem with deformed image

--- Vinarija.Web/app/services/blogService.js
+++ Vinarija.Web/app/services/blogService.js
@@ -31,9 +31,9 @@
     factory.buildPreviewDate = function (posts) {
         for (var i = 0; i < posts.length; i++) {
             if (posts[i].postImages && posts[i].postImages.length) {
-                posts[i].imageUrl = config.contentAddress + 'Posts\\' + posts[i].id + '\\' + posts[i].postImages[0].filePath
+                posts[i].imageUrl = config.contentAddress + 'Posts/' + posts[i].id + '/' + posts[i].postImages[0].filePath;
             }
-            posts[i].dateDay =  moment.utc(posts[i].date).local().format("DD");
+            posts[i].dateDay = moment.utc(posts[i].date).local().format("DD");
             posts[i].dateMonth = factory.monthsLocale[moment.utc(posts[i].date).local().format("MMM")];
         }
     }
