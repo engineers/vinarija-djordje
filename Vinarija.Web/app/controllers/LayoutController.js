1efb113321e4d7ccd18782a0c442af744a08c9fb
engineers-zorica <zorica@enginee.rs>
2016-09-28 17:12:10 +0200

    Add animations

--- Vinarija.Web/app/controllers/LayoutController.js
+++ Vinarija.Web/app/controllers/LayoutController.js
@@ -25,4 +25,59 @@
         var slide = parseInt(slide);
         $('#active-header-img').slick('slickGoTo', slide);
     };
+
+    //$scope.headerAnimation = {
+    //    offset: 0,
+    //    transitionTime: '1500',
+    //    targetOpacity: 1,
+    //    transformation: 'fromScale',
+    //    translateValue: '50px',
+    //    scaleValue: '0.9'
+    //};
+
+    $scope.headlineAnimation = {
+        offset: 150,
+        transitionTime: '1000',
+        targetOpacity: 1,
+        transformation: 'fromScale',
+        translateValue: '50px',
+        scaleValue: '0.5'
+    };
+
+    $scope.bodyTextAnimation = {
+        offset: 150,
+        transitionTime: '1000',
+        transformation: 'fromBottom',
+        translateValue: '50%',
+        targetOpacity: 1
+    };
+
+    $scope.rightAnimation = {
+        offset: 500,
+        targetOpacity: 1,
+        transitionTime: '500',
+        transformation: 'fromRight',
+        translateValue: '50px',
+    };
+
+    $scope.leftAnimation = {
+        offset: 350,
+        targetOpacity: 1,
+        transitionTime: '500',
+        transformation: 'fromLeft',
+        translateValue: '50px',
+    };
+
+    //$scope.circle = {
+    //    offset: 150,
+    //    transitionTime: '500',
+    //    transformation: 'fromScale',
+    //    scaleValue: '0.7'
+    //};
+
+    $scope.fadeInAnimation = {
+        offset: 300,
+        targetOpacity: 1,
+        transitionTime: '1700',
+    };
 });
---
+++ Vinarija.Web/app/directives/scrollTriger.js
@@ -0,0 +1,85 @@
+﻿app.directive("scrollTrigger", function ($window, $timeout) {
+    return {
+        restrict: 'A',
+        scope: {
+            animationSettings: '=',
+            animationDelay: '='
+        },
+        link: function (scope, element, attrs) {
+            if ($(window).width() > 1024) {
+                if (scope.animationSettings.targetOpacity) {
+                    element.css({
+                        'opacity': '0',
+                    });
+                }
+            }
+            if ($(window).width() > 1024) {
+                switch (scope.animationSettings.transformation) {
+                    case 'fromLeft':
+                        element.css({
+                            'transform': 'translateX(-' + scope.animationSettings.translateValue + ')'
+                        });
+                        break;
+                    case 'fromRight':
+                        element.css({
+                            'transform': 'translateX(' + scope.animationSettings.translateValue + ')'
+                        });
+                        break;
+                    case 'fromTop':
+                        element.css({
+                            'transform': 'translateY(-' + scope.animationSettings.translateValue + ')'
+                        });
+                        break;
+                    case 'fromBottom':
+                        element.css({
+                            'transform': 'translateY(' + scope.animationSettings.translateValue + ')'
+                        });
+                    case 'fromScale':
+                        element.css({
+                            'transform': 'scale(' + scope.animationSettings.scaleValue + ')'
+                        });
+                        break;
+                    default:
+                }
+            }
+
+            var visibility = false;
+            if ($(window).width() > 1024) {
+                var scrollOffset = scope.animationSettings.offset;
+            }
+
+            executeAnimation();
+            $($window).bind("scroll", executeAnimation);
+
+
+            function executeAnimation() {
+                var scrollPosition = $(window).scrollTop() + $(window).height() - scrollOffset - 150;
+                var elementPosition = element.offset().top;
+                if (scrollPosition > elementPosition) {
+                    visibility = true;
+                }
+
+                if (visibility && scope.animationSettings.transformation) {
+                    $timeout(function () {
+                        element.css({
+                            'opacity': scope.animationSettings.targetOpacity,
+                            'transform': 'translateY(0) translateX(0) scale(1)',
+                            '-webkit-transition': 'all ' + scope.animationSettings.transitionTime + 'ms',
+                            'transition': 'all ' + scope.animationSettings.transitionTime + 'ms'
+                        });
+                    }, scope.animationDelay);
+                }
+                else if (visibility) {
+                    $timeout(function () {
+                        element.css({
+                            'opacity': scope.animationSettings.targetOpacity,
+                            '-webkit-transition': 'all ' + scope.animationSettings.transitionTime + 'ms',
+                            'transition': 'all ' + scope.animationSettings.transitionTime + 'ms'
+                        });
+                    }, scope.animationDelay);
+                }
+            }
+        }
+    };
+});
+
