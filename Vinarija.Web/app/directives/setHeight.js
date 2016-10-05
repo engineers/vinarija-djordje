54d69f591cace960b3624630339a8cb6cd5d9b69
engineers-zorica <zorica@enginee.rs>
2016-10-05 11:35:37 +0200

    Styling

--- Vinarija.Web/app/directives/setHeight.js
+++ Vinarija.Web/app/directives/setHeight.js
@@ -1,9 +1,19 @@
 ﻿app.directive('setHeight', function () {
     return function (scope, element, attrs) {
         element.height($(window).height());
+        var windowWidth = $(window).width();
 
         $(window).bind('resize', function () {
-            element.height($(window).height());
+
+            if ($(window).width() > 767) {
+                element.height($(window).height());
+            }
+            else {
+                if ($(window).width() != windowWidth) {
+                    windowWidth = $(window).width();
+                    element.height($(window).height());
+                }
+            };
         });
     }
 });
