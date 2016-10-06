7aa76a07a93109efab454c271b281f23e6931030
Nikola Zivkovic <nikola.z@enginee.rs>
2016-10-06 13:44:26 +0200

    CP

--- Vinarija.Cms/app/sections/gallery/modals/ImagePreviewModalController.js
+++ Vinarija.Cms/app/sections/gallery/modals/ImagePreviewModalController.js
@@ -1,7 +1,6 @@
 ﻿'use strict';
 app.controller('ImagePreviewModalController', function ($scope, $mdDialog, config, image, $timeout) {
     $scope.image = image;
-    console.log($scope.image);
 
     $scope.cancel = function () {
         $mdDialog.cancel();
